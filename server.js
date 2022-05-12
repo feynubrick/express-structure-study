476cf9d30f77f057cb5f73c79962a98ce49b6ad1
Seung-Yoon Oh <feynubrick@gmail.com>
2022-05-12 14:50:57 +0900

    router, middleware 사용

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+require('express-async-errors');
+const router = require('./router');
+const middlewares = require('./middlewares');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(middlewares.requestLogger);
+app.use('/', router);
+app.use(middlewares.errorLogger);
+app.use(middlewares.errorResponser);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
